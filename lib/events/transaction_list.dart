950837c797cbc6a1a34041dc70ead70a6510a31f
abiy <abeyabx123123@gmail.com>
2021-09-02 12:30:55 +0300

    minor changes

--- lib/events/transaction_list.dart
+++ lib/events/transaction_list.dart
@@ -11,55 +11,80 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 400 + 100,
-      child: transaction.isEmpty
-          ? Column(
-              children: <Widget>[
-                Text('No transaction yet',
-                    style: Theme.of(context).textTheme.headline6),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                    height: 200,
-                    child: Image.asset('../assets/images/no_d.png',
-                        fit: BoxFit.cover))
-              ],
-            )
-          : ListView.builder(
-              itemBuilder: (ctx, index) {
-                return Card(
-                  elevation: 5,
-                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                        padding: EdgeInsets.all(6),
-                        child: FittedBox(
-                            child: Text(
-                                '\$${transaction[index].participantAmount.toStringAsFixed(2)}')),
-                      ),
-                    ),
-                    title: Text(transaction[index].title,
-                        style: Theme.of(context).textTheme.headline6),
-                    subtitle: Text(
-                      DateFormat.yMMMd().format(transaction[index]
-                          .date), //'yyyy-MM-dd date constructor
-                      style: TextStyle(color: Colors.blueGrey),
-                    ),
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete_outline),
-                      color: Theme.of(context).errorColor,
-                      onPressed: () => deleteTx(transaction[index].id),
-                    ),
-                  ),
-                );
-              },
-              itemCount: transaction.length,
-              //  children: transaction.map((tx) {}).toList(),
+    return Column(
+      children: [
+        Row(
+          children: [
+            SizedBox(
+              width: 10,
             ),
+            Column(
+              children: [
+                Text('No of'),
+                Text(
+                  'Participants',
+                ),
+              ],
+            ),
+            SizedBox(
+              width: 50,
+            ),
+            Text(
+              'Title',
+            )
+          ],
+        ),
+        Container(
+          height: 500,
+          child: transaction.isEmpty
+              ? Column(
+                  children: <Widget>[
+                    Text('No transaction yet',
+                        style: Theme.of(context).textTheme.headline6),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                        height: 200,
+                        child: Image.asset('../assets/images/no_d.png',
+                            fit: BoxFit.cover))
+                  ],
+                )
+              : ListView.builder(
+                  itemBuilder: (ctx, index) {
+                    return Card(
+                      elevation: 5,
+                      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          radius: 30,
+                          child: Padding(
+                            padding: EdgeInsets.all(6),
+                            child: FittedBox(
+                                child: Text(
+                                    '${transaction[index].participantAmount.toStringAsFixed(0)}')),
+                          ),
+                        ),
+                        title: Text(transaction[index].title,
+                            style: Theme.of(context).textTheme.headline6),
+                        subtitle: Text(
+                          DateFormat.yMMMd().format(transaction[index]
+                              .date), //'yyyy-MM-dd date constructor
+                          style: TextStyle(color: Colors.blueGrey),
+                        ),
+                        trailing: IconButton(
+                          icon: Icon(Icons.delete_outline),
+                          color: Theme.of(context).errorColor,
+                          onPressed: () => deleteTx(transaction[index].id),
+                        ),
+                      ),
+                    );
+                  },
+                  itemCount: transaction.length,
+                  //  children: transaction.map((tx) {}).toList(),
+                ),
+        ),
+      ],
     );
   }
 }
