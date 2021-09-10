8d8e86c0d69c49b777965866211ad380dd922181
DesmondSol <soltig66@gmail.com>
2021-09-10 10:16:53 +0300

    start on mysqli and separete hive and mysqli files

---
+++ lib/participants_hive/Participants_Home.dart
@@ -0,0 +1,144 @@
+import './transaction_list.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import './new_transaction.dart';
+import 'package:flutter/material.dart';
+
+import 'boxes.dart';
+import 'models/transaction.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  MyHomeePage createState() => MyHomeePage();
+}
+
+class MyHomeePage extends State<MyHomePage> {
+  @override
+  void dispose() {
+    Hive.close(); //close all
+    // Hive.box('Participants').close();
+    super.dispose();
+  }
+
+  // final List<Transaction> _userTran = [
+  //   Transaction(
+  //       id: 1,
+  //       name: 'Solomon Tigabu',
+  //       email: 'soltig66@gmail.com',
+  //       phoneNo: 0923214663,
+  //       qrCode: '000000'),
+  //   //   Transaction(
+  //   //       id: '2', title: 'New slippers', amount: 9.9, date: DateTime.now()),
+  //   //   Transaction(
+  //   //       id: '3', title: 'New phone', amount: 149.9, date: DateTime.now())
+  // ];
+
+  void _addNewTransa(
+    String participantName,
+    String email,
+    int phoneNo,
+  ) {
+    int b=0;
+    // if (_userTran.isEmpty) {
+    //   b = 1;
+    //   //print(b);
+    // } else {
+    //   b = _userTran.last.id + 1;
+    // }
+    String qrCode = participantName + b.toString();
+    // print('$b $txtitle $txamount');
+    final newTx = Transaction(
+      id: b,
+      name: participantName,
+      email: email,
+      phoneNo: phoneNo,
+      qrCode: qrCode,
+    );
+
+    // setState(() {
+    //   _userTran.add(newTx);
+    // });
+
+    final box = Boxes.getTransactions();
+    box.add(newTx); //I am here
+  }
+
+  void deleteTransaction(int id) {
+    setState(() {
+   //   _userTran.removeWhere((element) => element.id.toInt() == id);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext context) {
+    showModalBottomSheet<void>(
+        context: context,
+        builder: (BuildContext context) {
+          //  return NewTransaction(_addNewTransa);
+          return GestureDetector(
+            onTap: () {},
+            child: NewTransaction(_addNewTransa),
+            behavior: HitTestBehavior.opaque,
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          //actionsIconTheme: IconThemeData(color: Colors.white10, size: 36),
+          backgroundColor: Theme.of(context).primaryColor,
+          actions: <Widget>[
+            IconButton(
+                icon: const Icon(Icons.add),
+                tooltip: 'Add',
+                onPressed: () {
+                  _startAddNewTransaction(context);
+                }
+                //     _startAddNewTransaction(co): Builder(
+                // builder: (context) => _startAddNewTransaction(context)
+                ),
+            IconButton(
+              icon: const Icon(Icons.account_box_outlined),
+              tooltip: 'About creators',
+              onPressed: () {
+                final snackBar = SnackBar(
+                  content: Text(
+                    'made by Solomon T \n contact me: soltig66@gmail.com',
+                    textAlign: TextAlign.center,
+                  ),
+                  backgroundColor: Theme.of(context).primaryColor,
+                  //      duration: Duration(milliseconds: 200),
+                );
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              },
+            ),
+          ],
+          leading: Icon(Icons.access_time_sharp),
+          leadingWidth: 70,
+          title: const Text('Participants'),
+          //  centerTitle: true,
+        ),
+        body: SingleChildScrollView(
+            //for full screen scroll
+            child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // TransactionList(_userTran, deleteTransaction),
+            ValueListenableBuilder<Box<Transaction>>(
+                valueListenable: Boxes.getTransactions().listenable(),
+                builder: (context, box, _) {
+                  final transactions = box.values.toList().cast<Transaction>();
+                  return TransactionList(transactions, deleteTransaction);
+                })
+          ],
+        )),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: Builder(
+          builder: (context) => FloatingActionButton(
+            onPressed: () => _startAddNewTransaction(context),
+            child: Icon(Icons.add),
+          ),
+        ));
+  }
+}
