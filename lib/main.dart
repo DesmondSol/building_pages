b82cdc754b89c910bd664abc133a8a28d3e25b19
DesmondSol <soltig66@gmail.com>
2021-09-15 22:34:22 +0300

    resetting

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
-import 'package:build_pages/participants/models/transaction.dart';
+
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
-import 'package:hive_flutter/hive_flutter.dart';
+
 import 'participants/Participants_Home.dart';
 
-Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Hive.initFlutter();
-  Hive.registerAdapter(TransactionAdapter());
-  await Hive.openBox<Transaction>('Participants');
+void main() {
+//async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Hive.initFlutter();
+//  // Hive.registerAdapter(TransactionAdapter());
+//   await Hive.openBox<Transaction>('Participants');
   runApp(MyApp());
 }
 
